084541151b57c2f673a08a799a9895671f405a67
Jacek Gizinski <jacektrg+github@gmail.com>
2023-01-22 18:33:51 +0100

    Adding routes for uploading single file.
    Handling multer errors, calling next route handler.
    Returning file download URL back to client.

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const { uploadFile } = require('../multer/multer');
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-	res.send('respond with a resource');
+// Save uploaded file.
+router.post('/single-file', (req, res, next) => {
+	uploadFile.single('file')(req, res, (error) => {
+		if (error instanceof multer.MulterError) {
+			return res.status(error.errno).json(error);
+		} else if (error) {
+			return res.status(400).json('Error saving file.');
+		}
+		next();
+	});
+});
+
+// Return URL to download file.
+router.post('/single-file', (req, res, next) => {
+	const fileURL = `${process.env.API_URI}/public/files/${req.body.fileDir}/${req.file.originalname}`;
+	// Set timer to delete folder
+	return res.status(200).json(fileURL);
 });
 
 module.exports = router;
