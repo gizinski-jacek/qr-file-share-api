a7a40544c8872c831348785d5b178a496084ca7c
Jacek Gizinski <jacektrg+github@gmail.com>
2023-01-27 22:37:02 +0100

    Updating error code responses and messages.
    Adding route handling multiple files upload.
    Updating upload directory path.
    Removing redundant route handler.

--- routes/api.js
+++ routes/api.js
@@ -1,75 +1,92 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { uploadFile } = require('../multer/multer');
+const { singleFile, multipleFiles } = require('../multer/multer');
 const fs = require('fs');
 const path = require('path');
 const { nanoid } = require('nanoid');
 
-// Save uploaded file from PC.
+// Save single uploaded file from PC.
 router.post('/send-single-file', (req, res, next) => {
-	uploadFile.single('file')(req, res, (error) => {
+	singleFile.single('file')(req, res, (error) => {
 		if (error instanceof multer.MulterError) {
-			return res.status(error.errno).json(error);
+			return res.status(500).json(error.message);
 		} else if (error) {
-			return res.status(400).json('Error saving file.');
+			return res.status(500).json('Error saving file.');
 		}
-		next();
+		const fileURL = path.resolve(
+			`./public/uploads/${req.body.fileDir}/${req.file.originalname}`
+		);
+		return res.status(200).json(fileURL);
 	});
 });
 
-// Return URL to download file with phone.
-router.post('/send-single-file', (req, res, next) => {
-	const fileURL = `${process.env.API_URI}/api/public/files/${req.body.fileDir}/${req.file.originalname}`;
-	// Set timer to delete folder
-	return res.status(200).json(fileURL);
+// Save multiple uploaded files from PC.
+router.post('/send-multiple-files', (req, res, next) => {
+	multipleFiles.array('files')(req, res, (error) => {
+		if (error instanceof multer.MulterError) {
+			return res.status(500).json(error.message);
+		} else if (error) {
+			return res.status(500).json('Error saving files.');
+		}
+		const filesURLs = req.files.map((file) =>
+			path.resolve(`./public/uploads/${req.body.fileDir}/${file.originalname}`)
+		);
+		return res.status(200).json(filesURLs);
+	});
 });
 
-// Create directory for file.
-router.get('/receive-single-file', (req, res, next) => {
-	let dir = nanoid(6);
+// Create directory for phone file(s) uploads.
+router.get('/receive-files', (req, res, next) => {
+	let newDir = nanoid(6);
 	try {
-		while (!fs.accessSync(`public/files/${dir}`)) {
-			dir = nanoid(6);
+		while (!fs.accessSync(`public/uploads/${newDir}`)) {
+			newDir = nanoid(6);
 		}
 	} catch (err) {
 		if (err.code === 'ENOENT') {
-			fs.mkdirSync(`public/files/${dir}`, { recursive: true });
-			return res.status(200).json(dir);
+			fs.mkdirSync(`public/uploads/${newDir}`);
+			return res.status(200).json(newDir);
 		} else {
 			return res.status(400).json('Error creating directory.');
 		}
 	}
 });
 
-// Save uploaded file from phone.
-router.post('/receive-single-file/:dirId', (req, res, next) => {
-	uploadFile.single('file')(req, res, (error) => {
+// Save uploaded file(s) from phone.
+router.post('/receive-files/:dirId', (req, res, next) => {
+	multipleFiles.array('files')(req, res, (error) => {
 		if (error instanceof multer.MulterError) {
-			return res.status(error.errno).json(error);
+			return res.status(500).json(error.message);
 		} else if (error) {
-			return res.status(400).json('Error saving file.');
+			return res.status(500).json('Error saving file(s).');
+		} else {
+			// Set timer to delete folder.
+			// Notify correct socket.io client of uploaded files.
+			return res.status(200).json({ success: true });
 		}
-		// Set timer to delete folder
-		return res.status(200);
 	});
 });
 
 // Check if directory exists.
-router.get('/code-check-dir/:dirId', (req, res, next) => {
+router.get('/code-dir-check/:dirId', (req, res, next) => {
 	const { dirId } = req.params;
-	if (!fs.accessSync(`public/files/${dirId}`)) {
-		return res.status(200);
+	if (!fs.accessSync(`public/uploads/${dirId}`)) {
+		return res.status(200).json({ success: true });
 	} else {
 		return res.status(400).json('No directory.');
 	}
 });
 
 // Return requested file.
-router.get('/public/files/:dirId/:fileId', (req, res, next) => {
+router.get('/public/uploads/:dirId/:fileId', (req, res, next) => {
 	const { dirId, fileId } = req.params;
-	const file = path.resolve(`./public/files/${dirId}/${fileId}`);
-	return res.download(file);
+	if (!fs.accessSync(`public/uploads/${dirId}`)) {
+		const file = path.resolve(`./public/uploads/${dirId}/${fileId}`);
+		return res.download(file);
+	} else {
+		return res.status(400).json('No directory or file.');
+	}
 });
 
 module.exports = router;
