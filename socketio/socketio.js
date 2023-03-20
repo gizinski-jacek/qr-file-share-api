6d341f04326c82d065ed95046415bbb131f7fd65
Jacek Gizinski <jacektrg+github@gmail.com>
2023-03-20 21:15:54 +0100

    Renaming namespace.
    Fixing socket alert emit.

--- socketio/socketio.js
+++ socketio/socketio.js
@@ -3,12 +3,13 @@ const { Server } = require('socket.io');
 const client_list = [];
 
 const io = new Server();
-const client = io.of('/notifications');
+const notifications = io.of('/notifications');
 
-client.on('connection', (socket) => {
+notifications.on('connection', (socket) => {
 	if (!client_list.find((client) => client.id == socket.id)) {
 		client_list.push(socket);
 	}
+
 	socket.join(socket.handshake.query.code);
 
 	socket.on('disconnect', () => {
@@ -21,13 +22,7 @@ client.on('connection', (socket) => {
 
 const socketEmits = {
 	new_file_alert: (roomId, data) => {
-		// io.to(roomId).emit('new_file_alert', data);
-		const roomClients = client_list.filter((client) => {
-			if (Array.from(client.rooms).find((id) => id === roomId)) return client;
-		});
-		roomClients.forEach((socket) => {
-			io.to(socket.id).emit('new_file_alert', data);
-		});
+		notifications.to(roomId).emit('new_file_alert', data);
 	},
 };
 
