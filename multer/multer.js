ca8cae7b2b06e827b5caefac11d03d7988b0f0af
Jacek Gizinski <jacektrg+github@gmail.com>
2023-03-30 19:24:45 +0200

    Removing single file multer config.

--- multer/multer.js
+++ multer/multer.js
@@ -2,34 +2,7 @@ const fs = require('fs');
 const multer = require('multer');
 const { nanoid } = require('nanoid');
 
-const storageSingleFile = multer.diskStorage({
-	destination: async (req, file, cb) => {
-		const { dirId } = req.body;
-		if (dirId) {
-			cb(null, `public/uploads/${dirId}`);
-		} else {
-			let newDir = nanoid(6);
-			try {
-				while (!fs.accessSync(`public/uploads/${newDir}`)) {
-					newDir = nanoid(6);
-				}
-			} catch (err) {
-				if (err.code === 'ENOENT') {
-					fs.mkdirSync(`public/uploads/${newDir}`, { recursive: true });
-				} else {
-					cb(err);
-				}
-			}
-			req.body.fileDir = newDir;
-			cb(null, `public/uploads/${newDir}`);
-		}
-	},
-	filename: (req, file, cb) => {
-		cb(null, file.originalname);
-	},
-});
-
-const storageMultipleFiles = multer.diskStorage({
+const storage = multer.diskStorage({
 	destination: async (req, file, cb) => {
 		const { dirId } = req.params;
 		const { fileDir } = req.body;
@@ -55,18 +28,14 @@ const storageMultipleFiles = multer.diskStorage({
 		}
 	},
 	filename: (req, file, cb) => {
-		cb(null, file.originalname);
+		const uidName = Date.now() + '___' + file.originalname;
+		cb(null, uidName);
 	},
 });
 
-const singleFile = multer({
-	storage: storageSingleFile,
+const uploadFiles = multer({
+	storage: storage,
 	limits: { fileSize: 2000000 },
 });
 
-const multipleFiles = multer({
-	storage: storageMultipleFiles,
-	limits: { fileSize: 2000000 },
-});
-
-module.exports = { singleFile, multipleFiles };
+module.exports = { uploadFiles };
