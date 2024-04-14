89eaa174371e2f83168bb612574c48d5336aec62
Jacek Gizinski <jacektrg+github@gmail.com>
2024-04-14 20:51:15 +0200

    Refactoring directory existence check and file naming.
    Adding timestamp suffix to newly created directories.
    Removing timestamp from file names.
    Allowing duplicate files by adding number suffix to filename if file already exists in directory.

--- multer/multer.js
+++ multer/multer.js
@@ -1,35 +1,45 @@
 const fs = require('fs');
 const multer = require('multer');
 const { nanoid } = require('nanoid');
+const { directoryExists } = require('../lib/utils');
 
 const storage = multer.diskStorage({
 	destination: async (req, file, cb) => {
-		const { dirId } = req.params;
-		const { fileDir } = req.body;
-		if (fileDir) {
-			cb(null, `public/uploads/${fileDir}`);
-		} else if (dirId) {
-			cb(null, `public/uploads/${dirId}`);
+		const dirId = req.params.dirId || req.body.fileDir;
+		const dirExists = directoryExists(dirId);
+		if (dirExists) {
+			cb(null, `public/uploads/${dirExists}`);
 		} else {
 			let newDir = nanoid(6);
-			try {
-				while (!fs.accessSync(`public/uploads/${newDir}`)) {
-					newDir = nanoid(6);
-				}
-			} catch (err) {
-				if (err.code === 'ENOENT') {
-					fs.mkdirSync(`public/uploads/${newDir}`, { recursive: true });
-				} else {
-					cb(err);
-				}
+			const timestamp = Date.now();
+			while (directoryExists(newDir)) {
+				newDir = nanoid(6);
 			}
+			newDir = newDir + '___' + timestamp;
+			fs.mkdirSync(`public/uploads/${newDir}`, { recursive: true });
+
 			req.body.fileDir = newDir;
 			cb(null, `public/uploads/${newDir}`);
 		}
 	},
 	filename: (req, file, cb) => {
-		const uidName = Date.now() + '___' + file.originalname;
-		cb(null, uidName);
+		const dirId = req.params.dirId || req.body.fileDir;
+		const dirExists = directoryExists(dirId);
+		if (!dirExists) return cb('err');
+		let fileName = file.originalname;
+		const originalName = file.originalname.slice(
+			0,
+			file.originalname.lastIndexOf('.')
+		);
+		const extension = file.originalname.slice(
+			file.originalname.lastIndexOf('.')
+		);
+		let i = 1;
+		while (fs.existsSync(`public/uploads/${dirExists}/${fileName}`)) {
+			fileName = originalName + `_(${i})` + extension;
+			i++;
+		}
+		cb(null, fileName);
 	},
 });
 
