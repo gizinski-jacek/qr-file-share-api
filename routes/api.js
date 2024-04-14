8491c6cccb2d158ed303023066e2e3ef72ea4ff0
Jacek Gizinski <jacektrg+github@gmail.com>
2024-04-14 20:54:27 +0200

    Moving utility function reading directory files to separate module.
    Adding directory id (without timestamp) to response in POST send-files route allowing for redirect to the folder in frontend.
    Importing new function checking for existence of directory, replacing fs.accessSync checks with it.

--- routes/api.js
+++ routes/api.js
@@ -6,28 +6,9 @@ const fs = require('fs');
 const path = require('path');
 const { nanoid } = require('nanoid');
 const { socketEmits } = require('../socketio/socketio');
+const { readDirFilesDetails, directoryExists } = require('../lib/utils');
 
-// Set timer in redis to fire off worker handling file/folder deletion. !!!
-
-const readDirFilesDetails = (dir) => {
-	const files = fs.readdirSync(`public/uploads/${dir}`);
-	const fileList = files.map((fileName) => {
-		const fileSizeInBytes = fs.statSync(
-			`public/uploads/${dir}/${fileName}`
-		).size;
-		const name = fileName.slice(fileName.indexOf('___') + 3);
-		const extension = fileName.slice(fileName.lastIndexOf('.'));
-		return {
-			name: name,
-			size: fileSizeInBytes,
-			extension: extension,
-			url: `${process.env.API_URI}/api/public/uploads/${dir}/${fileName}`,
-		};
-	});
-	return fileList;
-};
-
-// Save uploaded files from PC.
+// Save uploads from PC.
 router.post('/send-files', (req, res, next) => {
 	uploadFiles.array('files')(req, res, (error) => {
 		if (error instanceof multer.MulterError) {
@@ -37,28 +18,25 @@ router.post('/send-files', (req, res, next) => {
 		}
 		const { fileDir } = req.body;
 		const fileList = readDirFilesDetails(fileDir);
-		return res.status(200).json(fileList);
+		const code = fileDir.split('___')[0];
+		return res.status(200).json({ fileList, code: code });
 	});
 });
 
-// Create directory for phone file(s) uploads.
-router.get('/receive-files', async (req, res, next) => {
+// Create directory for uploads from phone.
+router.get('/receive-files', (req, res, next) => {
 	let newDir = nanoid(6);
-	try {
-		while (!fs.accessSync(`public/uploads/${newDir}`)) {
-			newDir = nanoid(6);
-		}
-	} catch (err) {
-		if (err.code === 'ENOENT') {
-			fs.mkdirSync(`public/uploads/${newDir}`);
-			return res.status(200).json(newDir);
-		} else {
-			return res.status(400).json('Error creating directory.');
-		}
+	while (directoryExists(newDir)) {
+		newDir = nanoid(6);
 	}
+	const timestamp = Date.now();
+	fs.mkdirSync(`public/uploads/${newDir + '___' + timestamp}`, {
+		recursive: true,
+	});
+	return res.status(200).json(newDir);
 });
 
-// Save uploaded file(s) from phone.
+// Save uploads from phone.
 router.post('/receive-files/:dirId', (req, res, next) => {
 	uploadFiles.array('files')(req, res, (error) => {
 		if (error instanceof multer.MulterError) {
@@ -67,39 +45,39 @@ router.post('/receive-files/:dirId', (req, res, next) => {
 			return res.status(500).json('Error saving file(s).');
 		} else {
 			const { dirId } = req.params;
-			const fileList = readDirFilesDetails(dirId);
-			socketEmits.new_file_alert(dirId, fileList);
+			const dirExists = directoryExists(dirId);
+			const fileList = readDirFilesDetails(dirExists);
+			socketEmits.new_file_alert(dirExists, fileList);
 			return res.status(200).json({ success: true });
 		}
 	});
 });
 
-// Check if directory exists and return file list, create one if it does not.
-router.get('/code-dir-check/:dirId', async (req, res, next) => {
+// Check if directory exists and return file list, create one if it does not exist.
+router.get('/code-dir-check/:dirId', (req, res, next) => {
 	const { dirId } = req.params;
-	try {
-		if (!fs.accessSync(`public/uploads/${dirId}`)) {
-			const fileList = readDirFilesDetails(dirId);
-			return res.status(200).json(fileList);
-		}
-	} catch (err) {
-		if (err.code === 'ENOENT') {
-			fs.mkdirSync(`public/uploads/${dirId}`);
-			return res.status(200).json({ success: true });
-		} else {
-			return res.status(400).json('No directory. Error creating directory.');
-		}
+	const dirExists = directoryExists(dirId);
+	if (dirExists) {
+		const fileList = readDirFilesDetails(dirExists);
+		return res.status(200).json(fileList);
+	} else {
+		const timestamp = Date.now();
+		fs.mkdirSync(`public/uploads/${dirId + '___' + timestamp}`, {
+			recursive: true,
+		});
+		return res.status(200).json({ success: true });
 	}
 });
 
 // Return requested file.
-router.get('/public/uploads/:dirId/:fileId', (req, res, next) => {
-	const { dirId, fileId } = req.params;
-	if (!fs.accessSync(`public/uploads/${dirId}`)) {
-		const file = path.resolve(`./public/uploads/${dirId}/${fileId}`);
+router.get('/public/uploads/:dirId/:fileName', (req, res, next) => {
+	const { dirId, fileName } = req.params;
+	const dirExists = directoryExists(dirId);
+	if (dirExists) {
+		const file = path.resolve(`./public/uploads/${dirExists}/${fileName}`);
 		return res.download(file);
 	} else {
-		return res.status(400).json('No directory or file.');
+		return res.status(400).json('Directory or file does not exist.');
 	}
 });
 
